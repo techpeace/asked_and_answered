2b028e8ba21a15cd250f71f9d32beffaf574dad6
Matt Buck <techpeace@gmail.com>
2008-06-15 23:20:04 -0500

    Starting with blank Rails project.

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'f2950c3825e39a9d0c951f4b9fe77e24'
+end
